afc93989d0fdff4acc49f581384da43f9c0897d3
himakshis52 <himakshis52@gmail.com>
2024-04-28 22:34:13 +0530

    Navbar committed

---
+++ qtify/src/components/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Navbar from "..//Navbar/Navbar";
+
+
+function HomePage() {
+  
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default HomePage;
