0a6fe2aa025d00a5925c703014437b7f7047e9f5
himakshis52 <himakshis52@gmail.com>
2024-04-29 17:23:14 +0530

    Modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import "./App.css";
 import HomePage from './components/HomePage/HomePage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-    <HomePage />
-    </>
+    <Router>
+    <div className="App">
+          <div className="App-header">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+            </Routes>
+          </div>
+        </div>
+    </Router>
   );
 }
 
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,14 +1,50 @@
 import React from "react";
 import Navbar from "..//Navbar/Navbar";
 import Hero from "../Hero/Hero";
+import { useState, useEffect } from "react";
+import {
+    fetchTopAlbum,
+    fetchNewAlbum,
+    fetchSongs,
+    fetchGenres,
+  } from "../../BackendAPI/Api";
+import sectionStyles from "../Section/section.module.css";
+import Section from "../Section/Section";
 
 
 function HomePage() {
+
+    let [topAlbum, setTopAlbum] = useState([]);
+    let [newAlbum, setNewAlbum] = useState([]);
+    let [songs, setSongs] = useState([]);
+    let [genres, setGeneres] = useState([]);
+    useEffect(() => {
+      (async () => {
+        let topAlbumData = await fetchTopAlbum();
+        setTopAlbum(topAlbumData);
+  
+        let newAlbumData = await fetchNewAlbum();
+        setNewAlbum(newAlbumData);
+  
+        let fetchSongsData = await fetchSongs();
+        setSongs(fetchSongsData);
+  
+        let fetchSongsGenere = await fetchGenres();
+        setGeneres(fetchSongsGenere.data);
+      })();
+    }, []);
   
   return (
     <div>
-      <Navbar />
+      <Navbar data={topAlbum.concat(newAlbum)} page={"home"} />
       <Hero />
+      <div className={sectionStyles.sectionWrapper}>
+        <Section title="Top Albums" data={topAlbum} type="album" />
+        <Section title="New Albums" data={newAlbum} type="album" />
+        <hr />
+        <Section title="Songs" data={songs} type="songs" genres={genres} />{" "}
+        <hr />
+      </div>{" "}
     </div>
   );
 }
