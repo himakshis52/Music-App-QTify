4fc5e2695f8b4432dd36865db91b32e348fb225a
himakshis52 <himakshis52@gmail.com>
2024-04-29 09:37:46 +0530

    Hero section added

--- qtify/src/components/Button/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import navbarStyles from "../Navbar/Navbar.module.css";
-
-function Button({ text }){
-    return <button className={navbarStyles.feedback}>{ text }</button>
-}
-
-export default Button;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
             <h1> 100 Thousand Songs, ad - free </h1>
             <h1> Over thousands podcast episodes</h1>
             </div>
-            <img src={heroImg} alt="hero-Image" />
+            <img src={heroImg} alt="heroImage" />
         </div>
     )
 }
